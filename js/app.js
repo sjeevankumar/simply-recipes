f2f9fcb02b206c996b78f070e9f1ff199f057fb8
sjeevankumar <sjeevankumar89@gmail.com>
2024-05-07 14:26:52 +0530

    feat:add toggle functionality for navbar in mobile view

--- js/app.js
+++ js/app.js
@@ -1,19 +1,12 @@
 const getElement = (selector) => {
   const element = document.querySelector(selector)
-
   if (element) return element
-  throw Error(
-    `Please double check your class names, there is no ${selector} class`
-  )
+  throw Error(`Please double check your class names, there is no ${selector} class`)
 }
 
 const links = getElement('.nav-links')
 const navBtnDOM = getElement('.nav-btn')
 
-navBtnDOM.addEventListener('click', () => {
+navBtnDOM.addEventListener('click',()=>{
   links.classList.toggle('show-links')
 })
-
-const date = getElement('#date')
-const currentYear = new Date().getFullYear()
-date.textContent = currentYear
